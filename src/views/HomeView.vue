5fc348613ac97f240428713c1e767b47c77f9c25
dianadityaw <dian.aditya53@gmail.com>
2022-08-06 21:44:14 +0700

    add json data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,82 +1,122 @@
 <template>
   <div>
     <div class="header mb-4 mt-4">
-    <h1>
-      Diari Jajan Februari 2021
-    </h1>
-    <h5>
-      Pengeluaran Bulan Ini Rp. 5.000.000
-    </h5>
-    <b-button variant="primary">Tambah Item</b-button>
-    
+      <h1>Diari Jajan Februari 2021</h1>
+      <h5>Pengeluaran Bulan Ini Rp. 5.000.000</h5>
+      <b-button variant="primary">Tambah Item</b-button>
     </div>
-    <div class="card-note-list">
-      <b-card
-        header="18 Februari 2022"
-        header-tag="header"
-      >
-      <div class="list-content">
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <span class="me-4">03:15</span>
-            <span>Ayam Tangkap</span>
-          </div>
-          <p>Rp 32.154</p>
-        </div>
 
-  </div>
-    <div class="card-footer">
-      <strong>Total Rp 350.000</strong>
+    <div class="container">
+    <div class="card-note-list row">
+      <div class="col-3 mb-3" v-for="(item, idx) in detail" :key="idx">
+        <b-card
+          header="18 Februari 2022"
+          header-tag="header">
+          <div class="list-content">
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">{{ item.jam }}</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">03:15</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">03:15</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">03:15</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">03:15</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div>
+                <span class="me-3">03:15</span>
+                <span>Ayam Tangkap</span>
+              </div>
+              <p>Rp 32.154</p>
+            </div>
+          </div>
+          <div class="card-footer">
+            <strong>Total Rp 350.000</strong>
+          </div>
+      </b-card>
       </div>
-    </b-card>
+    </div>
+
     </div>
 </div>
 </template>
 
 <script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      detail: [
+        {
+            "jam": "03:15",
+            "tanggal":"18 Februari 2021",
+            "nama":"Ayam Tangkap",
+            "pengeluaraan":77249
+        },
+        {
+            "jam": "03:15",
+            "tanggal":"18 Februari 2021",
+            "nama":"Mie Rebus",
+            "pengeluaraan":32154
+        },
+        {
+            "jam": "00:20",
+            "tanggal":"17 Februari 2021",
+            "nama":"Soto Kuning",
+            "pengeluaraan":34992
+        },
+        {
+            "jam": "05:59",
+            "tanggal":"17 Februari 2021",
+            "nama":"Ayam Geprek",
+            "pengeluaraan":37674
+        },
+        {
+            "jam": "05:45",
+            "tanggal":"16 Februari 2021",
+            "nama":"Otak-otak",
+            "pengeluaraan":11971
+        }
+    ]
+    }
+  }
+}
  
 </script>
 
 <style lang="scss">
+.card-note-list .list-content > div {
 
+}
 .card-note-list {
-  margin : 1rem;
   .list-content {
+    font-size: 14px;
     > div {
       border-bottom: 1px solid lightgray;
     }
