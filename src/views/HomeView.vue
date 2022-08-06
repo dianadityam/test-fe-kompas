e8860a79304d85771a22af4a09f4c68d1f20f2f8
dianadityaw <dian.aditya53@gmail.com>
2022-08-06 21:06:51 +0700

    Add card note list UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,93 @@
 <template>
   <div>
+    <div class="header mb-4 mt-4">
     <h1>
       Diari Jajan Februari 2021
     </h1>
-    <h4>
+    <h5>
       Pengeluaran Bulan Ini Rp. 5.000.000
-    </h4>
+    </h5>
     <b-button variant="primary">Tambah Item</b-button>
+    
+    </div>
+    <div class="card-note-list">
     <b-card
-      header="featured"
+      header="18 Februari 2022"
       header-tag="header"
-      footer="Card Footer"
-      footer-tag="footer"
-      title="Title"
-    >
-      <b-card-text>Header and footers using props.</b-card-text>
-      <b-button href="#" variant="primary">Go somewhere</b-button>
+    ><div>
+    <table class="table">
+  <tbody>
+    <tr>
+      <td>03:15</td>
+      <td>Ayam Tangkap</td>
+      <td>Rp 77.249</td>
+    </tr>
+    <tr>
+      <td>04:52</td>
+      <td>Mie Rebus</td>
+      <td>Rp 32.154</td>
+    </tr>
+    <tr>
+      <td>06:46</td>
+      <td>Klontong</td>
+      <td>Rp 91.903</td>
+    </tr>
+    <tr>
+      <td>08:08</td>
+      <td>Urap</td>
+      <td>Rp 19.431</td>
+    </tr>
+    <tr>
+      <td>12:48</td>
+      <td>Kwetiau Rebus</td>
+      <td>Rp 53.890</td>
+    </tr>
+    <tr>
+      <td>19:09</td>
+      <td>Onde-Onde</td>
+      <td>Rp 42.120</td>
+    </tr>
+  </tbody>
+</table>
+
+  </div>
+    <div class="card-footer">
+      <strong>Total Rp 350.000</strong>
+      </div>
     </b-card>
+    </div>
 </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+ 
 </script>
 
 <style lang="scss">
+
+.card-note-list {
+  margin : 1rem;
+
+.card-footer {
+  text-align: right;
+  background-color: white;
+}
+
+.table-stripped {
+  color: white;
+}
+
+.table {
+  margin-bottom: 0%;
+}
+
+.card-header {
+  background-color: white;
+}
+}
+
+.header {
+  text-align: center;
+}
 
 </style>
